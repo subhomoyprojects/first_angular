5e7c67d18706104699f63211432aebc4e5ad0b74
Subhomoy Pal <subhomoyprojects@gmail.com>
2018-12-05 11:21:46 +0530

    Change path set in routing module.ts

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// Import Url Path
+import { Step1Component } from '../registration/step1/step1.component';
+import { from } from 'rxjs';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/registration/step1', pathMatch: 'full' },
+  { path: 'registration/step1', component: Step1Component }
+];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes,{
+    useHash:true
+  }) ],
   exports: [ RouterModule ]
-})
+  })
 export class AppRoutingModule {}
