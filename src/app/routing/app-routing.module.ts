f8f81ccb52114b1acda01ab62ef6c441df5855ac
Subhomoy Pal <subhomoyprojects@gmail.com>
2018-12-07 15:49:10 +0530

    Add Link of subscribtion

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // Import Url Path
 import { Step1Component } from '../registration/step1/step1.component';
+import { SubscriptionComponent } from '../registration/subscription/subscription.component';
 import { from } from 'rxjs';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/registration/step1', pathMatch: 'full' },
-  { path: 'registration/step1', component: Step1Component }
+  { path: 'registration/step1', component: Step1Component },
+  { path: 'registration/subscription', component: SubscriptionComponent }
 ];
 
 @NgModule({
