c5c4fd042c4e7995a0ef4ad14fa43e7f23717bd8
Subhomoy Pal <subhomoyprojects@gmail.com>
2018-12-11 13:19:20 +0530

    add owl related ts file

--- src/app/registration/addmoreinformation/addmoreinformation.component.ts
+++ src/app/registration/addmoreinformation/addmoreinformation.component.ts
@@ -14,4 +14,53 @@ export class AddmoreinformationComponent implements OnInit {
   
   isCollapsed = false; // Social Media Updown
 
+  // For Owl Carousal
+
+ owl1: any = {
+  loop: true,
+  mouseDrag: true,
+  touchDrag: false,
+  pullDrag: false,
+  dots: false,
+  navSpeed: 700,
+  margin:10,
+  navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+    400: {
+      items: 2
+    },
+    740: {
+      items: 3
+    }
+  },
+  nav: true
+}
+
+owl2: any = {
+  loop: true,
+  mouseDrag: true,
+  touchDrag: false,
+  pullDrag: false,
+  dots: false,
+  navSpeed: 700,
+  autoWidth:true,
+  margin:10,
+  navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+    400: {
+      items: 2
+    },
+    740: {
+      items: 4
+    }
+  },
+  nav: true
+}
+
 }
