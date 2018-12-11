892915884b28922f742cf0b968c3d1830af57e7c
Subhomoy Pal <subhomoyprojects@gmail.com>
2018-12-11 14:35:19 +0530

    add modal

--- src/app/registration/addmoreinformation/addmoreinformation.component.ts
+++ src/app/registration/addmoreinformation/addmoreinformation.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+// Ngb Modal
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-addmoreinformation',
   templateUrl: './addmoreinformation.component.html',
-  styleUrls: ['./addmoreinformation.component.css']
+  styleUrls: ['./addmoreinformation.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class AddmoreinformationComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) { 
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit() {
+  }
+
+  // For Ng Modal
+  open(open_big_image) {
+    this.modalService.open(open_big_image);
   }
   
   isCollapsed = false; // Social Media Updown
